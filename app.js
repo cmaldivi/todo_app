ac2c92b729a21697459b2a9ccd648e78428bcfaa
Augustin Trancart <augustin.trancart@phoxygen.com>
2015-09-22 20:35:04 +0200

    Add action

--- app.js
+++ app.js
@@ -1,3 +1,81 @@
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
+  'use strict';
+
+  window.taskAPI = (() => {
+    var tasks = [];
+
+    function getAllTasks() {
+        // TODO defeensively copy tasks
+      return Promise.resolve(tasks);
+    }
+
+    function addTask(description) {
+      var task = {description};
+      tasks.push(task);
+      return Promise.resolve(task);
+    }
+
+    return {
+      getAll: getAllTasks,
+      add: addTask
+    };
+  })();
+
+  window.taskUI = (() => {
+    var taskList = document.getElementById('tasklist');
+
+    function appendTask(task) {
+
+      var htmlTask = document.createElement('li');
+      // TODO sanitize user input
+      htmlTask.innerHTML = `<p>${task.description}</p>`
+      taskList.appendChild(htmlTask);
+    }
+
+    function refreshAllTask(tasks) {
+      refreshAll: (tasks) => {
+        taskList.innerHTML = '';
+
+        for (task of tasks) {
+          appendTask(task);
+        }
+      }
+    }
+
+    function showAddDialog() {
+      var dialog = document.querySelector('.addDialog');
+      dialog.hidden = false;
+      dialog.querySelector('input[name="description"]').value = '';
+      // hide drawer
+      window.location.hash = '';
+    }
+
+    function hideAddDialog() {
+      document.querySelector('.addDialog').hidden = true;
+    }
+
+    return {
+      append: appendTask,
+      refresh: refreshAllTask,
+      showAddDialog: showAddDialog,
+      hideAddDialog: hideAddDialog
+    }
+  })();
+
+  document.querySelector('.actionAdd').addEventListener('click', (e)=> {
+    e.preventDefault();
+    taskUI.showAddDialog();
+  });
+
+  document.querySelector('.addDialog > form button[type="submit"]')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    var desc = document.querySelector('.addDialog input[name="description"]');
+    taskAPI.add(desc.value).then(taskUI.append);
+    taskUI.hideAddDialog();
+
+  });
+
   console.log("Hello World!");
+  taskAPI.getAll().then(taskUI.refresh);
 });
