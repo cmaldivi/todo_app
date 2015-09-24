83e3265ff9426fbfdcd807bdb1023c7701bbbde1
cmaldivi <christophe.maldivi@gmail.com>
2015-09-24 12:22:18 +0200

    minor

--- app_install.js
+++ app_install.js
@@ -1,12 +1,13 @@
 window.addEventListener('load', function() {
   'use strict';
 
-navigator.mozApps.install("http://cmaldivi.github.io/todo_app/manifest.webapp")
-    .onSuccess() {
-      console.log("install ok");
-    }
-    .onError() {
-      console.log("install error");
-    };
-
-});
+var request = navigator.mozApps.install("http://cmaldivi.github.io/todo_app/manifest.webapp")
+request.onsuccess = function () {
+  // Enregistre l'objet App qui est renvoyé
+  var appRecord = this.result;
+  alert('Installation réussie !');
+};
+request.onerror = function () {
+  // Affiche le nom de l'erreur depuis l'objet DOMError
+  alert('Installation échouée, erreur : ' + this.error.name);
+};
