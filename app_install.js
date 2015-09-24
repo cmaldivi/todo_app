db67a4ac697028d3fc9999d3c81b09faf1877f50
cmaldivi <christophe.maldivi@gmail.com>
2015-09-24 12:20:20 +0200

    minor

--- app_install.js
+++ app_install.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', function() {
   'use strict';
 
-navigator.mozApps.install("http://cmaldivi.github.io/todo_app/manifest.webapp");
+navigator.mozApps.install("http://cmaldivi.github.io/todo_app/manifest.webapp")
+    .onSuccess() {
+      console.log("install ok");
+    }
+    .onError() {
+      console.log("install error");
+    };
 
 });
