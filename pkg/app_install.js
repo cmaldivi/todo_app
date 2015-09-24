946b0f21c0cf8c87cbe53101a2ea52647bb0aaa6
cmaldivi <christophe.maldivi@gmail.com>
2015-09-24 14:27:14 +0200

    minor

---
+++ pkg/app_install.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+  'use strict';
+
+var request = navigator.mozApps.install("https://cmaldivi.github.io/todo_app/pkg/manifest.webapp")
+request.onsuccess = function () {
+  // Enregistre l'objet App qui est renvoyé
+  var appRecord = this.result;
+  alert('Installation réussie !');
+};
+request.onerror = function () {
+  // Affiche le nom de l'erreur depuis l'objet DOMError
+  alert('Installation échouée, erreur : ' + this.error.name);
+};
+});
